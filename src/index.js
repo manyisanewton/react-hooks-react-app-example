3a5e6e4f302a5ba66db29f7a2dae44f8e3800fc6
Newton Manyisa <manyisanewton26@gmail.com>
2025-01-24 13:49:52 +0300

    done

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 import "./index.css";
+<script src="http://localhost:8097"></script>
 
 ReactDOM.render(<App />, document.getElementById("root"));
